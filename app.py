cf49f5d8a0b1263df4748221e0782474478c8a0b
praneeth pillarisetty <40534205+praneethpillarisetty@users.noreply.github.com>
2023-02-17 13:28:34 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ import authenticate as Auth
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-login_flag = False
+login_users = {}
+
+class login_user():
+    fb_user_obj = None
+    def set_fb_user(self, usr):
+        self.fb_user_obj = usr
+    def unset_fb_user(self):
+        self.fb_user_obj = None
+
 class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     email = db.Column(db.String(120))
     password = db.Column(db.String(80))
-
+    
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -31,12 +39,28 @@ def login():
         token, usr = Auth.firebaselogin(login.email, passw)
         if login is not None and (token):
             #return redirect(url_for("index"))
-            login_flag = True
-            return redirect(url_for("user_home",user = uname))
+            current_user = login_user()
+            current_user.set_fb_user(usr)
+            login_users[login.email] = current_user
+            return redirect(url_for("user_home", usr = uname))
         else:
             error = "Failed to Login Try Again!"
             return render_template("login.html", error = error)
     return render_template("login.html", error = error)
+
+@app.route("/<usr>/signout",methods=["GET", "POST"])
+def signout(usr):
+    error = None
+    if usr == 'None':
+        error = "Please login First"
+        return render_template("login.html", error = error)
+    else:
+        login = user.query.filter_by(username = usr).first()
+        del login_users[login.email]
+        error = "Signout Sucessfull!"
+        return render_template("login.html", error = error)
+    return render_template("login.html", error = error)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -55,9 +79,9 @@ def register():
             return render_template("register.html", error = error)
     return render_template("register.html", error = error)
 
-@app.route("/<user>/home", methods = ["GET", "POST"])
-def user_home(user):
-    return render_template("home.html", name = user)
+@app.route("/<usr>/home", methods = ["GET", "POST"])
+def user_home(usr):
+    return render_template("home.html", name = usr)
 
 if __name__ == "__main__":
     with app.app_context():
